47c26bfacd3e41becc015b5ec781919832c78d3d
lcurtis4 <lcurtis0104@gmail.com>
2021-07-06 14:47:11 -0500

    initial commit

---
+++ src/components/customers/CustomerCard.js
@@ -0,0 +1,9 @@
+import React from "react"  
+import "./Customer.css"
+
+export const CustomerCard = () => (
+    <section className="customer">
+        <h3 className="customer__name">John</h3>
+        <div className="customer__animal">Animal: Doodles</div>
+    </section>
+)
---
+++ src/components/employee/EmployeeCard.js
@@ -0,0 +1,9 @@
+import React from "react" 
+import "./Employee.css"
+
+export const EmployeeCard = () => (
+    <section className="employee">
+        <h3 className="employee__name">Bob</h3>
+        <div className="employee__shift">Shift: 8-5</div>
+    </section>
+)
